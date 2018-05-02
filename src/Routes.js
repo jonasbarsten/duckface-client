e9fe5ad6d16dc099302f03f889a48abe22cb0cdd
Jonas Barsten <jonasbarsten@gmail.com>
2018-05-02 16:46:00 +0200

    Completed initial tutorial

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home";
+
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
+
+import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
 
 export default ({ childProps }) =>
   <Switch>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <AppliedRoute path="/login" exact component={Login} props={childProps} />
-    <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    { /* /notes/new has to be avbove /notes/:id to be caught before we go into id-matching */ }
+    <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -9,3 +9,14 @@ export async function s3Upload(file) {
 
   return stored.key;
 }
+
+export async function s3Remove(key) {
+
+	console.log(key);
+
+  const removed = await Storage.vault.remove(key);
+
+  console.log(removed);
+
+  // return stored.key;
+}
